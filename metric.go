46d396dd8c31fba834967c4213f6bf4868e76bce
Gary Hetzel <garyhetzel@gmail.com>
2017-04-13 23:22:41 -0400

    Added metric bucketing

--- metric.go
+++ metric.go
@@ -18,6 +18,22 @@ type Point struct {
 	Value     float64   `json:"value"`
 }
 
+func (self Point) String() string {
+	return fmt.Sprintf("(%v, %f)", self.Timestamp, self.Value)
+}
+
+type PointSet []*Point
+
+func (self PointSet) String() string {
+	values := make([]string, len(self))
+
+	for i, point := range self {
+		values[i] = point.String()
+	}
+
+	return "\n"+strings.Join(values, "\n")
+}
+
 type IMetric interface {
 	SetName(string)
 	GetName() string
@@ -25,7 +41,7 @@ type IMetric interface {
 	GetTag(string) interface{}
 	GetTags() map[string]interface{}
 	SetTag(string, interface{})
-	GetPoints() []*Point
+	GetPoints() PointSet
 	Push(*Point)
 }
 
@@ -33,12 +49,12 @@ type Metric struct {
 	IMetric
 	name   string
 	tags   map[string]interface{}
-	points []*Point
+	points PointSet
 }
 
 func NewMetric(name string) *Metric {
 	metric := &Metric{
-		points: make([]*Point, 0),
+		points: make(PointSet, 0),
 	}
 
 	metric.SetName(name)
@@ -72,7 +88,7 @@ func (self *Metric) SetTag(key string, value interface{}) {
 	self.tags[key] = value
 }
 
-func (self *Metric) GetPoints() []*Point {
+func (self *Metric) GetPoints() PointSet {
 	return self.points
 }
 
@@ -101,6 +117,35 @@ func (self *Metric) MarshalJSON() ([]byte, error) {
 		`tags`:        self.GetTags(),
 		`points`:      self.GetPoints(),
 	})
+}
+
+func MakeTimeBuckets(points PointSet, duration time.Duration) []PointSet {
+	pointsets := make([]PointSet, 0)
+
+	if l := len(points); l > 0 {
+		currentBucket := make(PointSet, 0)
+		endOfBucket := points[l-1].Timestamp.Add(-1*duration)
+
+		for i := (l-1); i >= 0; i-- {
+			currentBucket = append(PointSet{points[i]}, currentBucket...)
+
+			if i > 0 {
+				if t := points[i-1].Timestamp; (t.Before(endOfBucket) || t.Equal(endOfBucket)) {
+					pointsets = append([]PointSet{currentBucket}, pointsets...)
+					currentBucket = nil
+
+					endOfBucket = t.Add(-1*duration)
+				}
+			}
+		}
+
+		if len(currentBucket) > 0 {
+			pointsets = append([]PointSet{currentBucket}, pointsets...)
+			currentBucket = nil
+		}
+	}
+
+	return pointsets
 }
 
 func SplitNameTags(name string, sep string) (string, map[string]interface{}) {
---
+++ metric_test.go
@@ -0,0 +1,43 @@
+package mobius
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+	"math/rand"
+)
+
+func TestPointBucketing(t *testing.T) {
+	assert := require.New(t)
+
+	metric := NewMetric(`mobius.test.pointbucket`)
+
+	for i := 0; i < 100; i++ {
+		metric.Push(&Point{
+			Timestamp: time.Date(2006, 1, 2, 15, 4, i, 0, mst),
+			Value:     0.1 + float64(rand.Float64() * float64(i)),
+		})
+	}
+
+	input := metric.GetPoints()
+	assert.Equal(100, len(input))
+
+	output := MakeTimeBuckets(input, 30*time.Second)
+	assert.Equal(4, len(output))
+
+	bucket := output[0]
+	assert.Equal(10, len(bucket))
+	assert.True(bucket[0].Timestamp.Before(bucket[len(bucket)-1].Timestamp))
+
+	bucket = output[1]
+	assert.Equal(30, len(bucket))
+	assert.True(bucket[0].Timestamp.Before(bucket[len(bucket)-1].Timestamp))
+
+	bucket = output[2]
+	assert.Equal(30, len(bucket))
+	assert.True(bucket[0].Timestamp.Before(bucket[len(bucket)-1].Timestamp))
+
+	bucket = output[3]
+	assert.Equal(30, len(bucket))
+	assert.True(bucket[0].Timestamp.Before(bucket[len(bucket)-1].Timestamp))
+}
